e11a49d5facf577219584b04299b06e552f2c759
michael <ei.dwaps@gmail.com>
2024-04-11 17:17:46 +0200

    sequelize

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = require("./routing");
 const app = express();
 const db = require("./db");
+
 db.sync()
   .then(() => {
     console.log("Database synced");
